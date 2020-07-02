a29b3cebf554b8be690841adbaeba1498db182b9
Goldenaries658 <Goldenaries658@users.noreply.github.com>
2020-07-02 19:32:33 +0000

    Prettified Code!

--- models/exercises.js
+++ models/exercises.js
@@ -1,30 +1,29 @@
-
 // sequelize (lowercase) references the connection to the DB in (index.js file)
 // const { sequelize } = require('index.js');
 
 // Creating the Exercise model
 module.exports = (sequelize, DataTypes) => {
-  const Exercise = sequelize.define('Exercise', {
+  const Exercise = sequelize.define("Exercise", {
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     muscle_gr: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     description: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     image: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     equipment: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
   });
   // logging Exercise just to pass lint tests
   console.log(Exercise);
--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 // Creating our User model
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+  const User = sequelize.define("User", {
     firstName: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     lastName: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
 
     // The email cannot be null, and must be a proper email before creation
@@ -19,14 +19,14 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true
-      }
+        isEmail: true,
+      },
     },
     // The password cannot be null
     password: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function (password) {
@@ -34,7 +34,7 @@ module.exports = (sequelize, DataTypes) => {
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook('beforeCreate', (user) => {
+  User.addHook("beforeCreate", (user) => {
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
