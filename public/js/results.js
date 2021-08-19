a51f638354768f94c2c8d36deafab1b008abc66d
Joe Dodgson <j.dodgson@live.co.uk>
2021-08-19 15:56:52 +0100

    Add comments to results.js file

--- public/js/results.js
+++ public/js/results.js
@@ -8,11 +8,16 @@ star.on('click', async event => {
     .attr('data-id');
 
   try {
+    // If the star was filled ('fa' class), then delete it from the user's favourite exercises 
     if ($(event.target).attr('class') === 'fa fa-star') {
       await $.ajax({ url: `/api/fave-exercise/${id}`, type: 'DELETE' });
+      // Change star styling from filled to unfilled
       $(event.target).attr('class', 'far fa-star');
+
+    // If the star was unfilled ('far' class), then add it to the user's favourite exercises
     } else {
       await $.post(`/api/fave-exercise/${id}`);
+      // Change star styling from unfilled to filled
       $(event.target).attr('class', 'fa fa-star');
     }
   } catch (err) {
