8316a34447de06895d1fdf17848114a78810fd8d
Joe Dodgson <j.dodgson@live.co.uk>
2021-09-09 21:48:01 +0100

    Display message to user to confirm they have added / removed an exercise from favourites

--- public/js/exerciseDetails.js
+++ public/js/exerciseDetails.js
@@ -1,6 +1,7 @@
 // Event listener for adding / removing a specific exercise from favourites
 const star = $('h3>.fa-star');
 const tooltipText = $('.tooltip-text');
+const exerciseChangeMsg = $('.exercise-change');
 
 star.on('click', async event => {
   event.preventDefault();
@@ -10,20 +11,34 @@ star.on('click', async event => {
 
   // When user clicks the star, add or remove exercise from favourites
   try {
-    // If the star was filled ('fa' class), then delete it from the user's favourite exercises
+    // If the star was filled ('fa' class), then delete exercise from user's favourites
     if ($(event.target).hasClass('fa')) {
       await $.ajax({ url: `/api/fave-exercise/${id}`, type: 'DELETE' });
       // Change star styling from filled to unfilled
-      $(event.target).removeClass('fa');
-      $(event.target).addClass('far');
-      tooltipText.text('Add to favourites');
-      // If the star was unfilled ('far' class), then add it to the user's favourite exercises
+      $(event.target)
+        .removeClass('fa')
+        .addClass('far');
+      // Update tooltip text
+      tooltipText
+        .text('Add to favourites');
+      // Update and display exercise change confirmation
+      exerciseChangeMsg
+        .text('Exercise removed from favourites')
+        .css('visibility', 'visible');
+      // If the star was unfilled ('far' class), then add exercise to user's favourites
     } else {
       await $.post(`/api/fave-exercise/${id}`);
       // Change star styling from unfilled to filled
-      $(event.target).removeClass('far');
-      $(event.target).addClass('fa');
-      tooltipText.text('Remove from favourites');
+      $(event.target)
+        .removeClass('far')
+        .addClass('fa');
+      // Update tooltip text
+      tooltipText
+        .text('Remove from favourites');
+      // Update and display exercise change confirmation
+      exerciseChangeMsg
+        .text('Exercise added to favourites')
+        .css('visibility', 'visible');
     }
   } catch (err) {
     console.error(`ERROR - results.js - star.on('click'): ${err}`);
